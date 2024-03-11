846992d823dedfac2bd4540698b0008b309951a0
6Midir9 <grand.zmievskiygmail.com>
2024-03-11 23:26:58 +0200

    2

--- components/urls.py
+++ components/urls.py
@@ -5,4 +5,7 @@ app_name = 'components'
 urlpatterns = [
      path('', views.index, name='index'),
      path('announcement/', views.announcement, name='announcement'),
+     path('new_announ', views.new_announ, name='new_announ'),
+     path('edit_announ/<int:announcement_id>/', views.edit_announ, name='edit_announ'),
+     path('delete_announ/<int:announcement_id>/', views.delete_announ, name='delete_announ')
 ]
--- components/views.py
+++ components/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Announcement
+from .forms import AnnounForm
 
 def index(request):
      return render(request, 'components/index.html')
@@ -7,3 +8,37 @@ def index(request):
 def announcement(request):
     announcements = Announcement.objects.all()
     return render(request, 'components/announcement.html', {'announcements': announcements})
+
+def new_announ(request):
+    form = AnnounForm(data=request.POST)
+
+    if request.method == 'POST':
+        form = AnnounForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('components:announcement')
+
+    context = {'form': form}
+    return render(request, 'components/new_announ.html', context)
+
+def edit_announ(request, announcement_id):
+     announcement = Announcement.objects.get(id=announcement_id)
+     if request.method != 'POST':
+          form = AnnounForm(instance=announcement)
+     else: 
+          form = AnnounForm(instance=announcement, data=request.POST)
+          if form.is_valid():
+             form.save()
+             return redirect('components:announcement')
+        
+     context = {'announcement' : announcement, 'form': form}
+     return render(request, 'components/edit_announ.html', context)
+
+def delete_announ(request, announcement_id):
+    announcement = Announcement.objects.get(pk=announcement_id)
+    if request.method == 'POST':
+        announcement.delete()
+        return redirect('components:announcement')
+    
+    context = {'announcement' : announcement}
+    return render(request, 'components/delete_announ.html', context)
